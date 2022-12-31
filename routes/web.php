c8f55789c0a9c58f1f63a02cea5892893d73a4d4
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-12-31 19:13:47 +0900

    wip

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,7 @@ Route::name('home')->group(function () {
 });
 
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
