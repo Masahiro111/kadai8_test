607dfdf59f810a980ddf047c63d8f901aa83d0c9
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-01-02 19:00:57 +0900

    wip

--- resources/views/auth/drafts/new.blade.php
+++ resources/views/auth/drafts/new.blade.php
@@ -2,10 +2,19 @@
     <div class="p-4">
         <form class="post-page-wrapper" action="/drafts/new" method="post">
             @csrf
-            <input type="text" class="block w-full mb-2" id="title-input" placeholder="タイトル" name="title">
-            <input type="text" class="block w-full mb-2" placeholder="プログラミング技術に関するタグをスペース区切りで3つまで入力" name="tags">
-            <div class="row">
-                <div class="">
+            <input
+                   type="text"
+                   class="mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                   id="title-input"
+                   placeholder="タイトル"
+                   name="title">
+            <input
+                   type="text"
+                   class="mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                   placeholder="プログラミング技術に関するタグをスペース区切りで3つまで入力"
+                   name="tags">
+            <div class="flex border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                <div class="w-1/2 h-auto">
                     <textarea
                               name="article"
                               id="markdown_editor_textarea"
@@ -13,7 +22,7 @@
                               rows="10"
                               class="w-full"></textarea>
                 </div>
-                <div class="">
+                <div class="w-1/2 h-auto bg-white">
                     <div id="markdown_preview"></div>
                 </div>
             </div>
@@ -22,4 +31,29 @@
             </div>
         </form>
     </div>
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+    <script>
+        $(function() {
+
+            // marked.setOptions({
+            //     langPrefix: '',
+            //     breaks : true,
+            //     sanitize: true,
+            // });
+
+            $('#markdown_editor_textarea').keyup(function() {
+                var html = marked.parse(getHtml($(this).val()));
+                $('#markdown_preview').html(html);
+            });
+
+            // 比較演算子が &lt; 等になるので置換
+            function getHtml(html) {
+                html = html.replace(/&lt;/g, '<');
+                html = html.replace(/&gt;/g, '>');
+                html = html.replace(/&amp;/g, '&');
+                return html;
+            }
+        });
+    </script>
 </x-app-layout>
